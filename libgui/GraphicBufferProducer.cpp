db064db6d68a524026bf2ce158a6d5ded00d9ad4
Ivan Vecera <ivan@cera.cz>
2023-06-26 13:10:07 +0200

    compat: Fix ABI issues
    Several constructor shims are implemented incorrectly because
    they completely ignore the fact that constructors have 'this-call'
    calling convention and should pass 'this ptr' as the first argument [1]
    as well as its return value, at least according ARM ABI [2].
    
    Additionally android::SurfaceComposerClient::Transaction::apply(bool)
    shim has void return value instead of status_t so the caller retrieves
    random value.
    
    [1] https://review.lineageos.org/c/LineageOS/android_device_xiaomi_sdm710-common/+/360011/comments/eb6600df_3e2fb434
    [2] https://github.com/ARM-software/abi-aa/blob/617079d8a0d45bec83d351974849483cf0cc66d5/cppabi32/cppabi32.rst?plain=1#L576
    
    Change-Id: I7b14b5d3ca6008c1e4b3f5fcbaece5021b3cbb82
    Signed-off-by: Ivan Vecera <ivan@cera.cz>

--- libgui/GraphicBufferProducer.cpp
+++ libgui/GraphicBufferProducer.cpp
@@ -11,12 +11,13 @@ using android::IBinder;
 using android::IGraphicBufferProducer;
 using android::sp;
 
-extern "C" void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
-        const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp,
+extern "C" void*
+_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
+        void* thisptr, const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp,
         const sp<IBinder>& surfaceControlHandle);
 
-extern "C" void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
-        const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp) {
-    _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
-            bufferProducer, controlledByApp, NULL);
+extern "C" void* _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb(
+        void* thisptr, const sp<IGraphicBufferProducer>& bufferProducer, bool controlledByApp) {
+    return _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
+            thisptr, bufferProducer, controlledByApp, nullptr);
 }
