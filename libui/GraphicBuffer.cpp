61b567bf0cf36cf2309af23102e6933606b035bc
R0rt1z2 <me@r0rt1z2.com>
2023-07-17 12:22:12 +0200

    libui: Add android::GraphicBufferMapper::lock shims
    Change-Id: I9f4da2bb1260d53ec4bb6604a89e78c495ad539a

--- libui/GraphicBuffer.cpp
+++ libui/GraphicBuffer.cpp
@@ -5,9 +5,11 @@
  */
 
 #include <stdint.h>
+#include <ui/GraphicBufferMapper.h>
+#include <ui/Rect.h>
 #include <utils/Errors.h>
 
-using android::status_t;
+namespace android {
 
 extern "C" {
 status_t _ZN7android13GraphicBuffer4lockEjPPvPiS3_(void* thisptr, uint32_t inUsage, void** vaddr,
@@ -17,4 +19,16 @@ status_t _ZN7android13GraphicBuffer4lockEjPPvPiS3_(void* thisptr, uint32_t inUsa
 status_t _ZN7android13GraphicBuffer4lockEjPPv(void* thisptr, uint32_t inUsage, void** vaddr) {
     return _ZN7android13GraphicBuffer4lockEjPPvPiS3_(thisptr, inUsage, vaddr, nullptr, nullptr);
 }
+
+status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
+        void* thisptr, buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr,
+        int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
+
+status_t _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(
+        void* thisptr, buffer_handle_t handle, uint32_t usage, const Rect& bounds, void** vaddr) {
+    return _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_(
+            thisptr, handle, usage, bounds, vaddr, nullptr, nullptr);
 }
+}
+
+}  // namespace android
