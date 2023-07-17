38d2cf97204b9736aaee5fe5e4cda3b942e10f07
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-17 12:11:44 +0200

    libui: Move every symbol under extern C block
    Change-Id: I19370532134f3ff84759177a1d6adcbd9da10a8d

--- libui/GraphicBuffer.cpp
+++ libui/GraphicBuffer.cpp
@@ -9,12 +9,12 @@
 
 using android::status_t;
 
-extern "C" status_t _ZN7android13GraphicBuffer4lockEjPPvPiS3_(void* thisptr, uint32_t inUsage,
-                                                              void** vaddr,
-                                                              int32_t* outBytesPerPixel,
-                                                              int32_t* outBytesPerStride);
+extern "C" {
+status_t _ZN7android13GraphicBuffer4lockEjPPvPiS3_(void* thisptr, uint32_t inUsage, void** vaddr,
+                                                   int32_t* outBytesPerPixel,
+                                                   int32_t* outBytesPerStride);
 
-extern "C" status_t _ZN7android13GraphicBuffer4lockEjPPv(void* thisptr, uint32_t inUsage,
-                                                         void** vaddr) {
+status_t _ZN7android13GraphicBuffer4lockEjPPv(void* thisptr, uint32_t inUsage, void** vaddr) {
     return _ZN7android13GraphicBuffer4lockEjPPvPiS3_(thisptr, inUsage, vaddr, nullptr, nullptr);
+}
 }
