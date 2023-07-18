a8100f6abcaf0ea6e2da339fbd381251c2d369d5
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-18 11:37:28 +0200

    compat: Add libtinyxml2 shim
    Change-Id: I2bc7963086f7f5df453e135819ed8b28c4d6a0d5

---
+++ libtinyxml2/XMLDocument.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <tinyxml2.h>
+#include <new>
+
+namespace tinyxml2 {
+
+extern "C" void _ZN8tinyxml211XMLDocumentC1Eb(XMLDocument* thisptr, bool processEntities) {
+    new (thisptr) XMLDocument(processEntities);
+}
+
+extern "C" XMLError _ZN8tinyxml211XMLDocument8SaveFileEPKc(XMLDocument* thisptr,
+                                                           const char* filename) {
+    return thisptr->SaveFile(filename);
+}
+
+}  // namespace tinyxml2
