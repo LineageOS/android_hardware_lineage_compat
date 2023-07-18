2ea181ff66c2fd659c1fd4c95fcf4d1bb7ba79b6
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-18 12:05:39 +0200

    [WIP] compat: Add vendor libsensor wrapper
    Change-Id: I62c27463af385dc791dde64731a39e9ebadfd587

---
+++ libsensor/SensorManager.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+extern "C" void _ZN7android13SensorManager21getInstanceForPackageERKNS_8String16E() {}
+extern "C" void _ZN7android13SensorManager16getDefaultSensorEi() {}
+extern "C" void _ZNK7android16SensorEventQueue13disableSensorEPKNS_6SensorE() {}
+extern "C" void _ZN7android16SensorEventQueue4readEP12ASensorEventm() {}
+extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8Ei() {}
+extern "C" void _ZNK7android16SensorEventQueue12enableSensorEPKNS_6SensorE() {}
+extern "C" void _ZNK7android16SensorEventQueue12setEventRateEPKNS_6SensorEl() {}
+extern "C" void _ZNK7android16SensorEventQueue12waitForEventEv() {}
+extern "C" void _ZN7android13SensorManager13getSensorListEPPKPKNS_6SensorE() {}
+extern "C" void _ZNK7android6Sensor7getNameEv() {}
