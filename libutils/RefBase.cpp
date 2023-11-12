084fe931d825081da59a8e3b8220ce851c55431b
LuK1337 <priv.luk@gmail.com>
2023-11-12 22:12:12 +0100

    compat: Add libutils_refbase_shim
    This shim overrides sp_report_stack_pointer() so that it's no longer
    fatal.
    
    Change-Id: I3f8ff85b1f6c713a311c06a50eeb7112e458a9ef

---
+++ libutils/RefBase.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+namespace android {
+
+void sp_report_stack_pointer() {}
+
+}  // namespace android
