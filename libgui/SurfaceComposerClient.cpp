beb5c4e069902de231db9f2752e3dd7c139d1349
Danny Trunk <dtrunk90@gmail.com>
2024-10-12 12:52:10 +0200

    compat: Add createDisplay() and destroyDisplay libgui shims
    Commit d53801c5273e34bc767e606454d32aa50f38ece8 renamed and modified
    SurfaceComposerClient::createDisplay and
    SurfaceComposerClient::destroyDisplay. Create a shim to provide the old
    symbol for blobs that need it.
    
    Change-Id: I4456976ef9cc19882b4a1fa31df7668c2c0b4c14

--- libgui/SurfaceComposerClient.cpp
+++ libgui/SurfaceComposerClient.cpp
@@ -12,7 +12,28 @@ extern "C" status_t _ZN7android21SurfaceComposerClient11Transaction5applyEbb(voi
                                                                              bool synchronous,
                                                                              bool oneWay);
 
+extern "C" void*
+_ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f(
+        void* displayName, bool isSecure, void* uniqueId, float requestedRefreshRate);
+
+extern "C" void* _ZN7android21SurfaceComposerClient6kEmptyE;
+
+extern "C" void _ZN7android21SurfaceComposerClient21destroyVirtualDisplayERKNS_2spINS_7IBinderEEE(
+        void* displayToken);
+
 extern "C" status_t _ZN7android21SurfaceComposerClient11Transaction5applyEb(void* thisptr,
                                                                             bool synchronous) {
     return _ZN7android21SurfaceComposerClient11Transaction5applyEbb(thisptr, synchronous, false);
+}
+
+extern "C" void* _ZN7android21SurfaceComposerClient13createDisplayERKNS_7String8Ebf(
+        void* displayName, bool isSecure, float requestedRefreshRate) {
+    return _ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f(
+            displayName, isSecure, _ZN7android21SurfaceComposerClient6kEmptyE,
+            requestedRefreshRate);
+}
+
+extern "C" void _ZN7android21SurfaceComposerClient14destroyDisplayERKNS_2spINS_7IBinderEEE(
+        void* displayToken) {
+    _ZN7android21SurfaceComposerClient21destroyVirtualDisplayERKNS_2spINS_7IBinderEEE(displayToken);
 }
