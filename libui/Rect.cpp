4ed78f9e2fb123b08864459c8247c8a2c68c3e74
LuK1337 <priv.luk@gmail.com>
2024-10-25 12:19:24 +0200

    compat: Add shims for ui::{HdrCapabilities,Rect,Size}
    Change-Id: I3f71236e821e203f599bc55d0ffbecf63936b825

---
+++ libui/Rect.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ui/Rect.h>
+
+std::string to_string(const android::Rect& rect) {
+    return android::to_string(rect);
+}
