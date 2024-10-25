76927c12e8f192dbb383436b606b787ffe30d823
LuK1337 <priv.luk@gmail.com>
2024-10-25 12:19:24 +0200

    compat: Add shims for ui::{HdrCapabilities,Rect,Size}
    Change-Id: I3f71236e821e203f599bc55d0ffbecf63936b825

---
+++ libui/HdrCapabilities.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <ui/GraphicTypes.h>
+
+namespace android {
+
+class HdrCapabilities {
+  public:
+    HdrCapabilities() = delete;
+    ~HdrCapabilities();
+
+    size_t getFlattenedSize() const;
+    status_t flatten(void* buffer, size_t size) const;
+    status_t unflatten(void const* buffer, size_t size);
+
+  private:
+    std::vector<ui::Hdr> mSupportedHdrTypes;
+    float mMaxLuminance;
+    float mMaxAverageLuminance;
+    float mMinLuminance;
+};
+
+}  // namespace android
