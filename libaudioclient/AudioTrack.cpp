4054ed74c959fd88d1f514d27c001ff0445d4409
bengris32 <bengris32@protonmail.ch>
2025-01-06 21:38:07 +0100

    compat: libaudioclient: Shim MTK constructor as well
    Change-Id: I55e0190121a506cf0c8e5c9dff7ecdaf99e627dc

--- libaudioclient/AudioTrack.cpp
+++ libaudioclient/AudioTrack.cpp
@@ -104,4 +104,20 @@ extern "C" void* newfnc(void* thisptr, audio_stream_type_t streamType, uint32_t 
                 selectedDeviceId);
 }
 
+#ifdef __LP64__
+extern "C" void*
+_ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tm20audio_output_flags_tRKNS_2wpINS0_19IAudioTrackCallbackEEEi15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfiRKNSt3__112basic_stringIcNSM_11char_traitsIcEENSM_9allocatorIcEEEE(
+        void* thisptr, audio_stream_type_t streamType, uint32_t sampleRate, audio_format_t format,
+        audio_channel_mask_t channelMask, size_t frameCount, audio_output_flags_t flags,
+        const wp<AudioTrack::IAudioTrackCallback>& callback, int32_t notificationFrames,
+        audio_session_t sessionId, AudioTrack::transfer_type transferType,
+        const audio_offload_info_t* offloadInfo, const AttributionSourceState& attributionSource,
+        const audio_attributes_t* pAttributes, bool doNotReconnect, float maxRequiredSpeed,
+        audio_port_handle_t selectedDeviceId, const std::string& /* unknown */) {
+    return orig(thisptr, streamType, sampleRate, format, channelMask, frameCount, flags, callback,
+                notificationFrames, sessionId, transferType, offloadInfo, attributionSource,
+                pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId);
+}
+#endif
+
 }  // namespace android
