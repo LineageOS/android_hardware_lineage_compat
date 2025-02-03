bffd7d2c44da5aad4022c4d5bb29db55ba531ad0
Mashopy <eliasgheeraert@gmail.com>
2025-02-03 22:43:49 +0100

    compat: libkeymaster: Squashed import of libkeymaster_shim
        maverick: Shim missing keystore symbol
    
        * The GenerateKeyResponse destructor was removed in commit [1].
    
        [1]: https://cs.android.com/android/_/android/platform/system/keymaster/+/c95ca65e11b1207a3e40fe09ef8a6d3b56a9dc76
    
        Signed-off-by: bengris32 <bengris32@protonmail.ch>
        Change-Id: Id7deacc92412ae9389fc9decf53b9dcb607c9d8b
    
        maverick: Update keymaster shim for Android 13
    
        * AttestKeyResponse's destructor was removed.
    
        Change-Id: Icd327a5dadc71f5d3458b3866bfb44a3bfd31a02
    
        jade: Shim missing keymaster symbol
    
        * copy_size_and_data_from_buf function was removed in Android 11
    
    Change-Id: I624159d68b8a8c1ee632fc2ac5c2bc643775fed6

---
+++ libkeymaster/GenerateKeyResponse.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// keymaster::GenerateKeyResponse::~GenerateKeyResponse()
+extern "C" void _ZN9keymaster19GenerateKeyResponseD1Ev() {
+    // no-op
+}
---
+++ libkeymaster/copy_size_and_data_from_buf.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// keymaster::copy_size_and_data_from_buf
+extern "C" void _ZN9keymaster27copy_size_and_data_from_bufEPPKhS1_PmPNS_9UniquePtrIA_hNS_13DefaultDeleteIS5_EEEE() {
+    // no-op
+}
