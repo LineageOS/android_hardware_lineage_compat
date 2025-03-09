6ae03a990f37f6374fe9b50993a610f429e9cd49
LuK1337 <priv.luk@gmail.com>
2025-03-09 09:17:27 +0100

    libbase: Shim old Trim function prototype
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I74641a068df88eb888473c8ceefc274fdf5332fc

--- libbase/strings.cpp
+++ libbase/strings.cpp
@@ -1,16 +1,19 @@
 /*
- * Copyright (C) 2023 The LineageOS Project
+ * Copyright (C) 2023-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <string.h>
-#include <string>
+#include <android-base/strings.h>
 
 namespace android {
 namespace base {
 bool StartsWith(const std::string& s, const char* prefix) {
     return strncmp(s.c_str(), prefix, strlen(prefix)) == 0;
+}
+
+std::string Trim(const std::string& s) {
+    return Trim(std::string_view(s));
 }
 }  // namespace base
 }  // namespace android
