88301981db7c6b057df1de72fb33a611f6dc812f
Bruno Martins <bgcngm@gmail.com>
2025-03-13 21:24:42 +0000

    compat: Add a shim for libprocessgroup
    DECLS removed as of https://cs.android.com/android/_/android/platform/system/core/+/3e7c17a8e7ad7275ea3a65642608b1c0becb38ad
    
    Change-Id: I9b2929e974db4e455d575aa962f337401c3e6a37

---
+++ libprocessgroup/processgroup.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ #include <string>
+
+extern "C" bool
+_Z23CgroupGetControllerPathRKNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEEPS5_(
+        const std::string& cgroup_name, std::string* path);
+
+extern "C" bool CgroupGetControllerPath(const std::string& cgroup_name, std::string* path) {
+    return _Z23CgroupGetControllerPathRKNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEEPS5_(
+            cgroup_name, path);
+}
