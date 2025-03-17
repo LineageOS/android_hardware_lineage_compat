09a4ed7a5f80ba8845c4bcae2f6e01cc7a542f22
Debayan Kar <kardebayan3@gmail.com>
2025-03-17 20:26:42 +0530

    vndk: Patch `libutils-v30` to load `libprocessgroup_shim`
    Change-Id: I4a66423d4c24fd368e2b7c532b058bb2c3465303

--- vndk/copy_libs.py
+++ vndk/copy_libs.py
@@ -49,7 +49,7 @@ for vndk_version, libs in {
                         ]
                     )
 
-                    if vndk_version == "v32" and lib == "libutils":
+                    if vndk_version in ["v30", "v32"] and lib == "libutils":
                         subprocess.run(
                             [
                                 PATCHELF_PATH,
@@ -58,7 +58,7 @@ for vndk_version, libs in {
                                 lib_dest,
                             ]
                         )
-
+                        
                     break
             else:
                 assert False, f"Failed to find {arch} {lib}"
