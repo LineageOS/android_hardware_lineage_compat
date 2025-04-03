340ab207b7871c18b89d7f33f70fb702dfb86d20
LuK1337 <priv.luk@gmail.com>
2025-04-03 09:03:30 +0200

    compat: Add libwfdservice_shim
    This is required for QCOM WFD compiled prior to 15 QPR3.
    
    Change-Id: Iaea06490d29a31791ca302b78a3f6eac990c6ff5

---
+++ libwfdservice/libwfdservice_shim.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <dlfcn.h>
+#include <media/AudioSystem.h>
+#include <media/DeviceDescriptorBase.h>
+
+using android::AudioSystem;
+using android::DeviceDescriptorBase;
+using android::sp;
+using android::media::AudioPortFw;
+using android::media::audio::common::AudioPort;
+
+struct WiFiDisplaySession {
+#ifdef __LP64__
+    char _pad1[30];
+#else
+    char _pad1[16];
+#endif
+    int flags;
+#ifdef __LP64__
+    char _pad2[90];
+#else
+    char _pad2[48];
+#endif
+    bool ready;
+    bool enabled;
+} __attribute__((packed));
+
+extern "C" int _ZN18WiFiDisplaySession31broadcastWifiDisplayAudioIntentEb(
+        WiFiDisplaySession* thisptr, bool enabled) {
+    if ((thisptr->flags & ~2) == 0) {
+        return 0;
+    }
+
+    if (enabled && !thisptr->ready) {
+        return -1;
+    }
+
+    thisptr->enabled = enabled;
+
+    AudioPortFw audioPortFw;
+    const auto devDescr = sp<DeviceDescriptorBase>::make(AUDIO_DEVICE_OUT_PROXY, "");
+    devDescr->setName("WFD proxy device");
+    devDescr->writeToParcelable(&audioPortFw);
+
+    AudioSystem::setDeviceConnectionState(
+            enabled ? AUDIO_POLICY_DEVICE_STATE_AVAILABLE : AUDIO_POLICY_DEVICE_STATE_UNAVAILABLE,
+            audioPortFw.hal, AUDIO_FORMAT_PCM);
+
+    return 0;
+}
