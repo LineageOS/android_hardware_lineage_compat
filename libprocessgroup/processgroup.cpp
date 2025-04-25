57ad414bd5daff0ed839e80b363f105e1d2a66df
chaptsand <chaptsand@gmail.com>
2025-04-25 12:06:51 +0800

    libprocessgroup: Add shim for CgroupGetAttributePath
    Some prebuilt binaries (e.g., vendor.qti.hardware.display.composer-service)
    reference CgroupGetAttributePath.
    
    Change-Id: I8bb8fdce6cbf2831dc0fb1242a6095e62d974172

--- libprocessgroup/processgroup.cpp
+++ libprocessgroup/processgroup.cpp
@@ -16,6 +16,14 @@ bool CgroupGetControllerPath(const std::string& cgroup_name, std::string* path) 
             cgroup_name, path);
 }
 
+bool _Z22CgroupGetAttributePathRKNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEEPS5_(
+        const std::string& attr_name, std::string* path);
+
+bool CgroupGetAttributePath(const std::string& attr_name, std::string* path) {
+    return _Z22CgroupGetAttributePathRKNSt3__112basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEEPS5_(
+            attr_name, path);
+}
+
 bool _Z15SetTaskProfilesiRKNSt3__16vectorINS_12basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEENS4_IS6_EEEEb(
         pid_t tid, const std::vector<std::string>& profiles, bool use_fd_cache = false);
 
