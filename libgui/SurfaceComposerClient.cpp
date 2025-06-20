9299c167d105c2030823050f547cdad7fe76cb3f
Skyblueborb <tomaszborbely0710@gmail.com>
2025-06-20 19:05:13 +0200

    libgui: Update createVirtualDisplay symbol for Android 25Q2
    Fixes:
    ld.lld: error: undefined symbol: _ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f
    >>> referenced by SurfaceComposerClient.cpp:31 (hardware/lineage/compat/libgui/SurfaceComposerClient.cpp:31)
    >>>               out/soong/.intermediates/hardware/lineage/compat/libgui_shim/android_arm64_armv8-a_cortex-a73_static/obj/hardware/lineage/compat/libgui/libgui_shim.so.lto.Surfac
    eComposerClient.o:(_ZN7android21SurfaceComposerClient13createDisplayERKNS_7String8Ebf)
    >>> did you mean: _ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbbS9_f
    >>> defined in: out/soong/.intermediates/frameworks/native/libs/gui/libgui/android_arm64_armv8-a_cortex-a73_shared/libgui.so
    
    Change-Id: I96e1c4c86dc69518d07ac0c3955d28217849b6a9

--- libgui/SurfaceComposerClient.cpp
+++ libgui/SurfaceComposerClient.cpp
@@ -13,8 +13,8 @@ extern "C" status_t _ZN7android21SurfaceComposerClient11Transaction5applyEbb(voi
                                                                              bool oneWay);
 
 extern "C" void*
-_ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f(
-        void* displayName, bool isSecure, void* uniqueId, float requestedRefreshRate);
+_ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbbS9_f(
+        void* displayName, bool isSecure, bool optimizeForPower, void* uniqueId, float requestedRefreshRate);
 
 extern "C" void* _ZN7android21SurfaceComposerClient6kEmptyE;
 
@@ -28,8 +28,8 @@ extern "C" status_t _ZN7android21SurfaceComposerClient11Transaction5applyEb(void
 
 extern "C" void* _ZN7android21SurfaceComposerClient13createDisplayERKNS_7String8Ebf(
         void* displayName, bool isSecure, float requestedRefreshRate) {
-    return _ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f(
-            displayName, isSecure, _ZN7android21SurfaceComposerClient6kEmptyE,
+    return _ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbbS9_f(
+            displayName, isSecure, true, _ZN7android21SurfaceComposerClient6kEmptyE,
             requestedRefreshRate);
 }
 
