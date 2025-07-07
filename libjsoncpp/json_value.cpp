b29c26e4df0484f0119e793f42b4b5bc7c35c850
Giovanni Ricca <giovanniricca@proton.me>
2025-07-07 07:59:52 +0200

    libjsoncpp: Define 64-bit integer constructors
    Change-Id: I67b4c504fd7c3dd01bc2d21f6dc3b568c8050f3f

--- libjsoncpp/json_value.cpp
+++ libjsoncpp/json_value.cpp
@@ -10,3 +10,13 @@ extern "C" Json::Value* _ZN4Json5ValueaSES0_(Json::Value* thisptr, Json::Value o
     *thisptr = other;
     return thisptr;
 }
+
+extern "C" Json::Value* _ZN4Json5ValueC1Ex(Json::Value* thisptr, Json::Int64 value) {
+    new (thisptr) Json::Value(value);
+    return thisptr;
+}
+
+extern "C" Json::Value* _ZN4Json5ValueC1Ey(Json::Value* thisptr, Json::UInt64 value) {
+    new (thisptr) Json::Value(value);
+    return thisptr;
+}
