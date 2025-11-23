df00450cc8aba1c3df52448a675b55e71ff1a056
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:16:56 +0100

    libinput: Provide A16 QPR0 android::MotionEvent::initialize() symbol
    Change-Id: Iab991b1c4fe9f73b5401df529981468461d5df2d

--- libinput/Input.cpp
+++ libinput/Input.cpp
@@ -1,9 +1,10 @@
 /*
- * Copyright (C) 2022-2024 The LineageOS Project
+ * Copyright (C) 2022-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <ftl/flags.h>
 #include <input/Input.h>
 #include <sys/types.h>
 #include <ui/LogicalDisplayId.h>
@@ -15,13 +16,30 @@ _ZN7android8KeyEvent10initializeEiijNS_2ui16LogicalDisplayIdENSt3__15arrayIhLm32
         int32_t scanCode, int32_t metaState, int32_t repeatCount, void* downTime, void* eventTime);
 
 extern "C" void
+_ZN7android11MotionEvent10initializeEiijNS_2ui16LogicalDisplayIdENSt3__15arrayIhLm32EEEiiNS_3ftl5FlagsINS_10MotionFlagEEEiiiNS_20MotionClassificationERKNS1_9TransformEffffSD_llmPKNS_17PointerPropertiesEPKNS_13PointerCoordsE(
+        int32_t id, int32_t deviceId, uint32_t source, android::ui::LogicalDisplayId displayId,
+        std::array<uint8_t, 32> hmac, int32_t action, int32_t actionButton,
+        android::ftl::Flags<android::MotionFlag> flags, int32_t edgeFlags, int32_t metaState,
+        int32_t buttonState, void* classification, void* transform, float xPrecision,
+        float yPrecision, float rawXCursorPosition, float rawYCursorPosition, void* rawTransform,
+        void* downTime, void* eventTime, size_t pointerCount, void* pointerProperties,
+        void* pointerCoords);
+
+extern "C" void
 _ZN7android11MotionEvent10initializeEiijNS_2ui16LogicalDisplayIdENSt3__15arrayIhLm32EEEiiiiiiNS_20MotionClassificationERKNS1_9TransformEffffS9_llmPKNS_17PointerPropertiesEPKNS_13PointerCoordsE(
         int32_t id, int32_t deviceId, uint32_t source, android::ui::LogicalDisplayId displayId,
         std::array<uint8_t, 32> hmac, int32_t action, int32_t actionButton, int32_t flags,
         int32_t edgeFlags, int32_t metaState, int32_t buttonState, void* classification,
         void* transform, float xPrecision, float yPrecision, float rawXCursorPosition,
         float rawYCursorPosition, void* rawTransform, void* downTime, void* eventTime,
-        size_t pointerCount, void* pointerProperties, void* pointerCoords);
+        size_t pointerCount, void* pointerProperties, void* pointerCoords) {
+    return _ZN7android11MotionEvent10initializeEiijNS_2ui16LogicalDisplayIdENSt3__15arrayIhLm32EEEiiNS_3ftl5FlagsINS_10MotionFlagEEEiiiNS_20MotionClassificationERKNS1_9TransformEffffSD_llmPKNS_17PointerPropertiesEPKNS_13PointerCoordsE(
+            id, deviceId, source, displayId, hmac, action, actionButton,
+            android::ftl::Flags<android::MotionFlag>{flags}, edgeFlags, metaState, buttonState,
+            classification, transform, xPrecision, yPrecision, rawXCursorPosition,
+            rawYCursorPosition, rawTransform, downTime, eventTime, pointerCount, pointerProperties,
+            pointerCoords);
+}
 
 extern "C" void _ZN7android8KeyEvent10initializeEiijiNSt3__15arrayIhLm32EEEiiiiiill(
         int32_t id, int32_t deviceId, uint32_t source, int32_t displayId,
