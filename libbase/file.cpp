4399d3db79eff2640780b43f9e628a62962da9b8
Sarthak Roy <sarthakroy2002@gmail.com>
2025-12-04 22:38:42 +0530

    libbase: Shim old Basename/Dirname function prototype
    - https://android.googlesource.com/platform/system/libbase/+/2902e107c0f16ab23ab2779deaf79417f23e028d%5E%21/#F0
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I2661d4ad982c17553cf72275f8f7a1391e366f7d

---
+++ libbase/file.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2023-2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/file.h>
+#include <android-base/strings.h>
+
+namespace android {
+namespace base {
+
+std::string Basename(const std::string &path) {
+  return Basename(std::string_view(path));
+}
+
+std::string Dirname(const std::string &path) {
+  return Dirname(std::string_view(path));
+}
+
+} // namespace base
+} // namespace android
