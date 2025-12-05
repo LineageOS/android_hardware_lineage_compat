4ddf94c927ee5c3a148732d4f35f4594f9ae0d85
althafvly <althafvly@murena.io>
2025-12-05 05:16:08 +0000

    compat: libpiex_shim: Add dng shim
    - com.qti.node.watermark.so: error: Unresolved symbol:
      _ZN13dng_area_task15ProcessOnThreadEjRK8dng_rectRK9dng_pointP17dng_abort_sniffer
    
    Change-Id: I5416e246de5dcb0faad64fdbae27871afc850f68

---
+++ libpiex/dng_shim.cpp
@@ -0,0 +1,19 @@
+class dng_rect;
+class dng_point;
+class dng_abort_sniffer;
+
+class dng_area_task {
+public:
+    void ProcessOnThread(unsigned int threadIndex,
+                         const dng_rect &area,
+                         const dng_point &tileSize,
+                         dng_abort_sniffer *sniffer);
+};
+
+void dng_area_task::ProcessOnThread(unsigned int,
+                                    const dng_rect &,
+                                    const dng_point &,
+                                    dng_abort_sniffer *)
+{
+    // no-op shim
+}
