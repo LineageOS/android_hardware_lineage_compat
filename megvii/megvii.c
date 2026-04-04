b046abf7ca84eec7f0c81142fe4a4d3d2d6b5506
Marc Bourgoin <themard85@gmail.com>
2026-04-04 12:40:18 -0600

    compat: megvii: Add libMegviiBeautyBody, libmegskeleton, and _meg_beauty_body_get_api_impl stub
    As seen on zippo
    
    Change-Id: I78787eaedb0896cf235f53e5fb0779a971d8d6cd

--- megvii/megvii.c
+++ megvii/megvii.c
@@ -18,3 +18,7 @@ void* mg_facepp[] = {
         &stub_fail, &stub_fail, &stub_fail, &stub_fail,     &stub_fail,     &stub_fail, &stub_fail,
         &stub_fail, &stub_fail, &stub_fail, &stub_fail,     &stub_fail,     &stub_fail,
 };
+
+void* _meg_beauty_body_get_api_impl(void) {
+    return NULL;
+}
